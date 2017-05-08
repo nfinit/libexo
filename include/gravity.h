60f1dd82405de4c798993d36d2d45120868170f9
ict <iantruslow@onyx.boisestate.edu>
2017-05-08 02:44:52 -0600

    new header file for gravity calculation functions

---
+++ include/gravity.h
@@ -0,0 +1,19 @@
+/* PWB GRAVITY UTILITIES HEADER
+ *
+ * Revision I (04/17) (C) NFINIT Systems 2017
+ * Author: ict (ict@nfinit.systems)
+ *
+ * This file provides function prototypes for the gravity
+ * utilities file that can be used in other programs utilizing
+ * these functions
+ *
+ * These functions work with the following units:
+ * Surface gravity: meters/second^2
+ * Length: meters
+ * Mass: kilograms
+ *
+ */
+
+double surface_gravity(double mass, double radius);
+double radius_sg(double mass, double sg);
+double mass_sg(double radius, double sg);
