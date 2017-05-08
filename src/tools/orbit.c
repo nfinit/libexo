a926b97ddb275115aed4a1aa07920be54f3570da
ict <iantruslow@onyx.boisestate.edu>
2017-05-08 15:59:14 -0600

    orbital characterization utility now supports calculating perihelion and aphelion distances from eccentricity

--- src/tools/orbit.c
+++ src/tools/orbit.c
@@ -1,13 +1,13 @@
 /* PWB COMMAND-LINE FRONTEND UTILITIES
  * Orbital characterization utility
  *
- * Revision I (04/17) (C) NFINIT Systems 2017
+ * Revision II (05/17) (C) NFINIT Systems 2017
  * Author: ict (ict@nfinit.systems)
  *
  * This program provides a simple front-end for the orbital computation
  * facilities of the Planetary Workbench, allowing for conversion between
- * orbital periods, semimajor axes, and combined host-satellite masses based
- * on known data.
+ * orbital periods, semimajor axes, combined host-satellite masses,
+ * aphelion and perihelion distances, based on known data.
  */
 
 #include <stdio.h>
@@ -24,29 +24,30 @@
        \
 <number> <unit> \
 <\"perihelion\"|\"aphelion\"> \
-<eccentricity> \'e\'"
+<eccentricity>"
 
 /* enumerations, prototypes and other useful definitions */
 enum conversion_type {sma_period, period_sma, e_peri, e_aph, conv_error};
-typedef enum conversion_type convtype;
-convtype get_conv(char *arg);
+typedef enum conversion_type conv_T;
+conv_T get_conv(char *arg);
 void print_unit_info();
 
 /* MAIN FUNCTION
  * Takes the following arguments:
  * <number> <unit> <host mass> <unit> <companion mass> <unit>
- * <number> <unit> <perihelion|aphelion> <eccentricity> 'e'
+ * <SmA> <unit> <perihelion|aphelion> <eccentricity>
  */
 int main (int argc, char *argv[])
 {
 	/* check that proper arguments have been supplied */
-	if (argc < 7 || argc > 7) 
+	if (argc < 5 || argc > 7) 
 	{
 		printf("%s\n",HELP_STR);
 		return 0;
 	}
 
 	/* variable declaration and initialization */
+	double e = 0.0; /* eccentricity */
 	double n = 0.0; /* primary value to evaluate */
 	double nc = 0.0; /* converted value (into seconds or meters) */
 	double hm = 0.0; /* host mass */
@@ -55,8 +56,7 @@ int main (int argc, char *argv[])
 	double cc = 0.0; /* companion mass in kilograms */
 	double r = 0.0; /* computation result */
 	double rc = 0.0; /* human-readable computation result */
-	convtype conv = conv_error; /* calculation/conversion to perform */
-	convtype conv_e = conv_error; /* check for eccentricity conversions */
+	conv_T conv = conv_error; /* calculation/conversion to perform */
 	length_T su = NO_SMA_UNIT; /* unit of the primary value if SmA */
 	time_T pu = NO_PER_UNIT; /* unit of the primary value if period */
 	mass_T hu = NO_MASS_UNIT; /* units of the host mass */
@@ -67,8 +67,8 @@ int main (int argc, char *argv[])
 	n = atof(argv[1]);
 
 	/* determine conversion type */
-	conv = get_conv(argv[2]);
-	conv_e = get_conv(argv[3]);
+	conv = get_conv(argv[3]); /* check for eccentricity */
+	if (conv == conv_error) conv = get_conv(argv[2]);
 
 	/* parse first unit of input */
 	switch (conv) {
@@ -78,6 +78,14 @@ int main (int argc, char *argv[])
 		case period_sma:
 			pu = parse_per_unit(argv[2]);
 			break;
+		case e_peri:
+			e = atof(argv[4]);
+			su = parse_sma_unit(argv[2]);
+			break;
+		case e_aph:
+			e = atof(argv[4]);
+			su = parse_sma_unit(argv[2]);
+			break;
 		default:
 			print_unit_info();		
 			printf("%s\n",HELP_STR);
@@ -85,16 +93,16 @@ int main (int argc, char *argv[])
 	}
 
 	/* parse first mass */
-	hm = atof(argv[3]);
+	if (!e) hm = atof(argv[3]);
 
 	/* parse first mass unit */
-	hu = parse_mass_unit(argv[4]);
+	if (!e) hu = parse_mass_unit(argv[4]);
 
 	/* parse second mass */
-	cm = atof(argv[5]);
+	if (!e) cm = atof(argv[5]);
 
 	/* parse second mass unit */
-	cu = parse_mass_unit(argv[6]);
+	if (!e) cu = parse_mass_unit(argv[6]);
 
 	/* convert primary value to proper argument for computation */
 	switch (conv) {
@@ -104,6 +112,12 @@ int main (int argc, char *argv[])
 		case period_sma:
 			nc = convert_time(n,pu,seconds);
 			break;
+		case e_peri:
+			nc = convert_length(n,su,m);
+			break;
+		case e_aph:
+			nc = convert_length(n,su,m);
+			break;
 		default:
 			print_unit_info();		
 			printf("%s\n",HELP_STR);
@@ -111,16 +125,22 @@ int main (int argc, char *argv[])
 	}
 
 	/* convert host/companion masses to kilograms */
-	hc = convert_mass(hm,hu,kg);
-	cc = convert_mass(cm,cu,kg);
+	if (!e) hc = convert_mass(hm,hu,kg);
+	if (!e) cc = convert_mass(cm,cu,kg);
 
 	/* compute result */
 	switch (conv) {
 		case sma_period:
-			r = period(nc,hc,cc);
+			r = period(nc, hc, cc);
 			break;
 		case period_sma:
-			r = sma(nc,hc,cc);
+			r = sma(nc, hc, cc);
+			break;
+		case e_peri:
+			r = perihelion(nc, e); 
+			break;
+		case e_aph:
+			r = aphelion(nc, e); 
 			break;
 		default:
 			print_unit_info();		
@@ -133,12 +153,20 @@ int main (int argc, char *argv[])
 		case sma_period:
 			pu = select_time_T(r);
 			out_str = time_T_string(pu);
-			rc = convert_time(r,seconds,pu);
+			rc = convert_time(r, seconds, pu);
 			break;
 		case period_sma:
 			su = select_length_T(r);
 			out_str = length_T_string(su);
-			rc = convert_length(r,m,su);
+			rc = convert_length(r, m, su);
+			break;
+		case e_peri:
+			out_str = length_T_string(su);
+			rc = convert_length(r, m, su); 
+			break;
+		case e_aph:
+			out_str = length_T_string(su);
+			rc = convert_length(r, m, su); 
 			break;
 		default:
 			print_unit_info();		
@@ -155,12 +183,12 @@ int main (int argc, char *argv[])
 
 /* Parses arguments to get a conversion type to pass to the main function
  */
-convtype get_conv(char *arg)
+conv_T get_conv(char *arg)
 {
 	/* declare variables */
 	length_T s = NO_SMA_UNIT;
 	time_T p = NO_PER_UNIT;
-	convtype r = conv_error;
+	conv_T r = conv_error;
 
 	/* attempt to parse an SMA first */ 
 	s = parse_sma_unit(arg);
@@ -176,6 +204,13 @@ convtype get_conv(char *arg)
 	/* check if result was valid */
 	if (p != NO_PER_UNIT) {
 		r = period_sma;
+	}
+
+	/* attempt to parse a perihelion/aphelion conversion */
+	if (strcmp(arg, "aphelion") == 0) {
+		r = e_aph;
+	} else if (strcmp(arg, "perihelion") == 0) {
+		r = e_peri;
 	}
 
 	/* if we get here, no valid unit was entered */
