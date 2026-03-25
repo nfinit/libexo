fe29b18d2ee55fc272d89ca5a55008660b8389e6
felix <kaszynskibolbo@gmail.com>
2026-03-25 15:10:09 +0200

    added coloured output in test.c and test.h

--- include/test.h
+++ include/test.h
@@ -9,4 +9,28 @@
  * This file provides definitions for the test suite of libexo library
  */
 
+// Defines the reset escape code
+#define RESET_EC "\033[0m"
+
+// Defines the colour of text escape codes
+#define RED_COL     "\033[31m"
+#define GREEN_COL   "\033[32m"
+#define YELLOW_COL  "\033[33m"
+#define BLUE_COL    "\033[34m"
+#define MAGENTA_COL "\033[35m"
+#define CYAN_COL    "\033[36m"
+#define WHITE_COL   "\033[37m"
+
+typedef enum {
+    RED,
+    GREEN,
+    YELLOW,
+    BLUE,
+    MAGENTA,
+    CYAN,
+    WHITE
+} text_colour;
+
+void print_col_text(text_colour col, char *str);
+
 #endif
--- test/test.c
+++ test/test.c
@@ -12,6 +12,24 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello World!\n");
+    print_col_text(RED, "test");
 }
 
+void print_col_text(text_colour col, char *str) {
+    switch (col) {
+        case RED: printf( "%s%s%s", RED_COL, str, RESET_EC);
+            break;
+        case GREEN: printf( "%s%s%s",GREEN_COL, str, RESET_EC);
+            break;
+        case YELLOW: printf( "%s%s%s",YELLOW_COL, str, RESET_EC);
+            break;
+        case BLUE: printf( "%s%s%s",BLUE_COL, str, RESET_EC);
+            break;
+        case MAGENTA: printf( "%s%s%s",MAGENTA_COL, str, RESET_EC);
+           break;
+        case CYAN: printf( "%s%s%s",CYAN_COL, str, RESET_EC);
+            break;
+        case WHITE: printf( "%s%s%s",WHITE_COL, str, RESET_EC);
+            break;
+    }
+}
